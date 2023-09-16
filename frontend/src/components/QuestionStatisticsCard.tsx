14e1ae51eca5db98ef71fe7f51210109ba4b01dd
tlyi <t.leyi2305@gmail.com>
2023-09-16 23:11:13 +0800

    build: install d3 package for chart visualisation

--- frontend/src/components/QuestionStatisticsCard.tsx
+++ frontend/src/components/QuestionStatisticsCard.tsx
@@ -1,5 +1,3 @@
-import QuestionTable from "./QuestionTable";
-
 const QuestionStatisticsCard = () => {
   return (
     <div className="flex flex-col h-full justify-center gap-4 items-center bg-black rounded-lg p-8">
