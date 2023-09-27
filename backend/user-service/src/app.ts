21e761c304a7647d15b74c49f7d075d6ed7201d8
tlyi <t.leyi2305@gmail.com>
2023-09-28 01:10:26 +0800

    feat: implement auth middleware on user service routes

--- backend/user-service/src/app.ts
+++ backend/user-service/src/app.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./routes";
 import bodyParser from "body-parser";
 import HttpStatusCode from "./lib/enums/HttpStatusCode";
 import cors from "./middleware/cors";
+import { authMiddleware } from "./middleware/auth";
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ app.use(cors);
 app.use(bodyParser.json());
 
 // implement routes for API endpoints
-app.use("/api", router);
+app.use("/api", authMiddleware, router);
 
 app.all("*", (req: Request, res: Response) => {
   res.status(HttpStatusCode.NOT_FOUND).json({
---
+++ backend/user-service/src/middleware/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import HttpStatusCode from "../lib/enums/HttpStatusCode";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.headers.bypass) {
+    const serviceSecret = process.env.SERVICE_SECRET || "secret";
+    // bypass auth for calls from auth service
+    if (req.headers.bypass === serviceSecret) {
+      next();
+      return;
+    }
+  }
+
+  const authRes = await fetch("http://localhost:5050/api/auth/validate", {
+    method: "POST",
+    headers: req.headers as HeadersInit,
+  });
+
+  if (authRes.status !== HttpStatusCode.OK) {
+    const message = await authRes.text();
+    res.status(authRes.status).json({
+      error: message,
+      message,
+    });
+    return;
+  }
+  next();
+};
