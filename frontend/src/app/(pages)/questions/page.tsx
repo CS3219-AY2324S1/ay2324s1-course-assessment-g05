daf400bd8efb821c2b7724c0126cf343433f4833
c0j0s <c0j0s@outlook.com>
2023-09-27 16:46:30 +0800

    Add problem description in question detail page.

--- frontend/src/app/(pages)/questions/page.tsx
+++ frontend/src/app/(pages)/questions/page.tsx
@@ -4,18 +4,12 @@ import QuestionTable from "@/components/question/QuestionTable";
 import { getQuestionList } from "@/helpers/question/question_api_wrappers";
 import MatchingLobby from "@/components/matching/MatchingLobby";
 
-export const metadata: Metadata = {
-  title: "Questions",
-  description: "coding questions",
-};
-
 export default async function QuestionsPage() {
   const questions: Question[] = await getQuestionList();
 
   return (
     <>
       <QuestionTable questions={questions}></QuestionTable>
-      <MatchingLobby></MatchingLobby>
     </>
   );
 }
