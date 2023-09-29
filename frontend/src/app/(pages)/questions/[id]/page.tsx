fd893d9b78d5682e2aedfd879c294f9269edd3fb
tryyang2001 <e0550472@u.nus.edu>
2023-09-29 16:59:01 +0800

    convert question detail page to client component so that it is able to be rendered

--- frontend/src/app/(pages)/questions/[id]/page.tsx
+++ frontend/src/app/(pages)/questions/[id]/page.tsx
@@ -1,30 +1,36 @@
+"use client";
+
 import { getQuestionById } from "@/helpers/question/question_api_wrappers";
 import Question from "@/types/question";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
-import parse from 'html-react-parser';
+import { Suspense, useEffect, useState } from "react";
 import ProblemDescription from "@/components/collab/ProblemDescription";
 
-async function getQuestion(id: string) {
-  const res = await getQuestionById(id, 'no-cache');
-
-  if ('title' in res) {
-    return res as Question;
-  } else {
-    notFound();
-  }
-}
-
-export default async function QuestionDetailPage({
+export default function QuestionDetailPage({
   params,
 }: {
-  params: { id: string }
+  params: { id: string };
 }) {
-  const question = await getQuestion(params.id);
+  const [question, setQuestion] = useState<Question>();
+
+  async function getQuestion(id: string) {
+    const res = await getQuestionById(id, "no-cache");
+
+    if ("title" in res) {
+      const question = res as Question;
+      setQuestion(question);
+    } else {
+      notFound();
+    }
+  }
+
+  useEffect(() => {
+    getQuestion(params.id);
+  }, []);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <ProblemDescription question={question} />
+      {question !== undefined && <ProblemDescription question={question} />}
     </Suspense>
   );
 }
