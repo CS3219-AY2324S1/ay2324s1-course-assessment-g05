27e9fa33b945a7f5dc9b1ea54b41e61610afcd43
tryyang2001 <e0550472@u.nus.edu>
2023-09-30 00:21:12 +0800

    add a default loading component in the app-level so that every loading state will render the loading component ui

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,6 @@
+import LogoLoadingComponent from "@/components/common/LogoLoadingComponent";
+import React from "react";
+
+export default function Loading() {
+  return <LogoLoadingComponent />;
+}
