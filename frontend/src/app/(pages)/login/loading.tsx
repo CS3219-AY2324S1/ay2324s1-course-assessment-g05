3289d2ec306236e2d0a34caa44006e9a3cc02dd0
tryyang2001 <e0550472@u.nus.edu>
2023-09-30 00:29:57 +0800

    remove unecessary loading.tsx files

--- frontend/src/app/(pages)/login/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LogoLoadingComponent from "@/components/common/LogoLoadingComponent";
-import React from "react";
-
-export default function Loading() {
-  return <LogoLoadingComponent />;
-}
