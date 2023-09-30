87ef7c81f1e98e128677e5f145383636b28d7b03
c0j0s <c0j0s@outlook.com>
2023-10-01 01:25:28 +0800

    Change getMatchedRecord to server action

--- frontend/src/helpers/matching/matching_api_wrappers.tsx
+++ frontend/src/helpers/matching/matching_api_wrappers.tsx
@@ -1,41 +1,36 @@
-/* -------------------------------------------------------------------------- */
-/*                      mock backend for matching service                     */
-/* -------------------------------------------------------------------------- */
 "use server"
-import { HTTP_METHODS, SERVICE } from "@/types/enums";
-import endpoint, { getSocketConfig } from "../endpoint";
-import { io } from "socket.io-client";
-import api from "../endpoint";
+import { SERVICE } from "@/types/enums";
+import { getSocketConfig } from "../endpoint";
 import { getLogger } from "../logger";
 
 const logger = getLogger("wrapper");
 
 //TODO: change preferences to be a type
-export async function submitMatchPreferences (preferences: {}) {
+export async function submitMatchPreferences(preferences: {}) {
   console.log(`Submitted for matching: ${JSON.stringify(preferences)}`);
 };
 
-export async function getMatchingSocket()  {
+export async function getMatchingSocket() {
   return await getSocketConfig(SERVICE.MATCHING);
 }
-// TODO: update this to an actual API call to matching service
-// const getMatchedRecord = ({
-//   firstUserId,
-//   secondUserId,
-//   questionId,
-//   matchedLanguage,
-// }: {
-//   firstUserId: string;
-//   secondUserId: string;
-//   questionId: string;
-//   matchedLanguage: string;
-// }) => {
-//   return {
-//     firstUserId: firstUserId,
-//     secondUserId: secondUserId,
-//     questionId: questionId,
-//     matchedLanguage: matchedLanguage,
-//   };
-// };
 
-// export const MatchingService = { submitMatchPreferences, getMatchedRecord };
+export async function getMatchedRecord(
+  {
+    firstUserId,
+    secondUserId,
+    questionId,
+    matchedLanguage,
+  }: {
+    firstUserId: string;
+    secondUserId: string;
+    questionId: string;
+    matchedLanguage: string;
+  }
+) {
+  return {
+    firstUserId: firstUserId,
+    secondUserId: secondUserId,
+    questionId: questionId,
+    matchedLanguage: matchedLanguage,
+  };
+}
