46cd224f464161b586e7efda7e48d8d1447d0d20
tlyi <t.leyi2305@gmail.com>
2023-10-07 13:39:00 +0800

    fix: remove collabcontext from problemdescription as it is a shared component

--- frontend/src/components/collab/ProblemPanel.tsx
+++ frontend/src/components/collab/ProblemPanel.tsx
@@ -1,9 +1,14 @@
-import ProblemDescription from "./ProblemDescription";
+import { useCollabContext } from "@/contexts/collab";
+import ProblemDescription from "../common/ProblemDescription";
 
 const ProblemPanel = () => {
+  const { question } = useCollabContext();
+
+  if (!question) return;
+
   return (
     <div className="h-[calc(100vh-60px)]">
-      <ProblemDescription />
+      <ProblemDescription question={question} />
     </div>
   );
 };
