673759f3f1e73933cbff2e930917b75e967d008e
tryyang2001 <e0550472@u.nus.edu>
2023-10-27 17:57:19 +0800

    support dynamically adjust the profile image in the dashboard according to window size

--- frontend/src/components/common/ProfilePictureAvatar.tsx
+++ frontend/src/components/common/ProfilePictureAvatar.tsx
@@ -1,19 +1,39 @@
-import { UserService } from "@/helpers/user/user_api_wrappers";
-import User from "@/types/user";
+import { cn } from "@/utils/classNameUtils";
 import { Avatar } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 interface ProfilePictureProps {
   profileUrl?: string;
-  size?: string;
+  isProfileDashboard?: boolean;
 }
 
 const ProfilePictureAvatar = ({
   profileUrl,
-  size = "9",
+  isProfileDashboard = false,
 }: ProfilePictureProps) => {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    if (!isProfileDashboard) {
+      return;
+    }
+
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+
+    return () => {
+      window.removeEventListener("resize", () =>
+        setWindowWidth(window.innerWidth)
+      );
+    };
+  }, []);
+
   return (
     <Avatar
-      className={`transition-transform w-${size} h-${size}`}
+      className={cn("transition-transform w-", {
+        "w-40 h-40": isProfileDashboard && windowWidth >= 1100,
+        "w-32 h-32": isProfileDashboard && windowWidth < 1100,
+        "w-9 h-9": !isProfileDashboard,
+      })}
       src={
         profileUrl || "https://cdn-icons-png.flaticon.com/512/149/149071.png"
       }
