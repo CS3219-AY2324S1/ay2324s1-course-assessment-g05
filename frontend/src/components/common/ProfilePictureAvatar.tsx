396a3cab20922d1daa82920034621234d196aba8
tryyang2001 <e0550472@u.nus.edu>
2023-10-27 18:54:37 +0800

    resolve build error

--- frontend/src/components/common/ProfilePictureAvatar.tsx
+++ frontend/src/components/common/ProfilePictureAvatar.tsx
@@ -5,11 +5,17 @@ import { useEffect, useState } from "react";
 interface ProfilePictureProps {
   profileUrl?: string;
   isProfileDashboard?: boolean;
+  isChatAvatar?: boolean;
+  isMatchingAvatar?: boolean;
+  isProfileAvatar?: boolean;
 }
 
 const ProfilePictureAvatar = ({
   profileUrl,
   isProfileDashboard = false,
+  isChatAvatar = false,
+  isMatchingAvatar = false,
+  isProfileAvatar = false,
 }: ProfilePictureProps) => {
   const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
@@ -32,7 +38,14 @@ const ProfilePictureAvatar = ({
       className={cn("transition-transform w-", {
         "w-40 h-40": isProfileDashboard && windowWidth >= 1100,
         "w-32 h-32": isProfileDashboard && windowWidth < 1100,
-        "w-9 h-9": !isProfileDashboard,
+        "w-8 h-8": isChatAvatar,
+        "w-16 h-16": isMatchingAvatar,
+        "w-12 h-12": isProfileAvatar,
+        "w-9 h-9":
+          !isProfileDashboard &&
+          !isChatAvatar &&
+          !isMatchingAvatar &&
+          !isProfileAvatar,
       })}
       src={
         profileUrl || "https://cdn-icons-png.flaticon.com/512/149/149071.png"
