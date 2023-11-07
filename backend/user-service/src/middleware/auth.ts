97a4079d7002c49bcff35add41a45b7a52c50ed5
tryyang2001 <e0550472@u.nus.edu>
2023-11-07 20:02:44 +0800

    create user service integration tests

--- backend/user-service/src/middleware/auth.ts
+++ backend/user-service/src/middleware/auth.ts
@@ -8,7 +8,6 @@ export const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
-  console.debug(`[${req.url}][${req.method}] ${JSON.stringify(req.params)}\n${JSON.stringify(req.body)}`);
   if (req.headers.bypass) {
     const serviceSecret = process.env.SERVICE_SECRET || "secret";
     // bypass auth for calls from auth service
@@ -33,16 +32,15 @@ export const authMiddleware = async (
   //If there is no JWT, do not need to go through auth
   if (!jwtCookieString) {
     res.status(HttpStatusCode.UNAUTHORIZED).json({
-      error: "Unauthorised",
+      error: "UNAUTHORISED",
       message: "Unauthorised",
     });
     return;
   }
 
-  const AUTH_GATEWAY = process.env.AUTH_GATEWAY || "http://localhost:5050"
+  const AUTH_GATEWAY = process.env.AUTH_GATEWAY || "http://localhost:5050";
   //If there is JWT, validate it through the auth endpoint
-  const authEndpoint =
-    process.env.AUTH_ENDPOINT || `auth/api/validate`;
+  const authEndpoint = process.env.AUTH_ENDPOINT || `auth/api/validate`;
 
   const authRes = await fetch(`${AUTH_GATEWAY}/${authEndpoint}`, {
     method: "POST",
@@ -50,12 +48,19 @@ export const authMiddleware = async (
       Cookie: `jwt=${jwtCookieString}`,
     },
   });
-  
-  console.debug(`[authMiddle][${authRes.status}] fetch ${AUTH_GATEWAY}/${authEndpoint}`);
+
   if (authRes.status !== HttpStatusCode.OK) {
+    if (authRes.status === HttpStatusCode.UNAUTHORIZED) {
+      res.status(HttpStatusCode.UNAUTHORIZED).json({
+        error: "UNAUTHORISED",
+        message: "Unauthorised",
+      });
+      return;
+    }
+
     const message = await authRes.text();
     res.status(authRes.status).json({
-      error: message,
+      error: message.toUpperCase(),
       message,
     });
     return;
