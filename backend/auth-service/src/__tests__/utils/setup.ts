926420d2a6666ccf409875f8448353ab187018de
tryyang2001 <e0550472@u.nus.edu>
2023-11-10 20:32:05 +0800

    remove extra printing

--- backend/auth-service/src/__tests__/utils/setup.ts
+++ backend/auth-service/src/__tests__/utils/setup.ts
@@ -3,9 +3,6 @@ import db from "../../lib/db";
 process.env.NODE_ENV = "test";
 
 export const updatePasswordResetToken = async (token: string) => {
-  console.log("Last 10 characters of the token", token.slice(-10));
-  console.log(process.env.TEST_AUTH_USER_ID?.slice(-4));
-  console.log(process.env.TEST_DATABASE_URL?.includes("-dev"));
   await db.user.update({
     where: {
       id: process.env.TEST_AUTH_USER_ID,
