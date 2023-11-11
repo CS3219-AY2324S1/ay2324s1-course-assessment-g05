9aadec8312bffbbe7c742d89706972c9c848a79e
c0j0s <c0j0s@outlook.com>
2023-11-12 04:01:55 +0800

    Simplify status response for matching-service

--- backend/matching-service/src/controllers/handlers/get-handler.ts
+++ backend/matching-service/src/controllers/handlers/get-handler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import HttpStatusCode from "../../lib/enums/HttpStatusCode";
 import RoomManager from "../../lib/utils/roomManager";
 
-const queueEndpoint = `http://${process.env.QUEUE_URL || "localhost"}:15672/api/overview`
+const queueEndpoint = `http://${process.env.QUEUE_URL || "localhost"}:15672/api`
 const queueAuthString = 'Basic ' + Buffer.from(`${process.env.QUEUE_ACCOUNT || 'guest'}:${process.env.QUEUE_PASSWORD || 'guest'}`).toString('base64');
 
 export const getHealth = async (_: Request, response: Response) => {
@@ -32,16 +32,18 @@ export const getHealth = async (_: Request, response: Response) => {
 export const getStatus = async (_: Request, response: Response) => {
   try {
     const rm = RoomManager.getInstance();
-    const queueResponse = await fetch(queueEndpoint, {
+    const queueResponse = await fetch(`${queueEndpoint}/queues`, {
       headers: {
         Authorization: queueAuthString,
       }
     });
 
+    const selectedFields = (await queueResponse.json()).map((queue: { name: any; message_stats: any; }) => ({ name: queue.name, message_stats: queue.message_stats }));
+    
     const data = {
       count: rm.count(),
       rooms: rm.list(),
-      queue: await queueResponse.json()
+      queue: selectedFields
     }
 
     response.status(HttpStatusCode.OK).json(data);
